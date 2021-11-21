4d710f6abfdd6aaadcdfb4e3cd3d0da346ad48eb
Valentin <valentin.monteiro@orange.fr>
2021-11-21 12:09:10 +0100

    Construction du parcours utilisateur

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => { 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+    next();
+});
+
+mongoose.connect('mongodb+srv://Valentin:1303@cluster0.qcuwr.mongodb.net/piiquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use('/api/auth', userRoutes);
+
+
+app.listen(3000);
